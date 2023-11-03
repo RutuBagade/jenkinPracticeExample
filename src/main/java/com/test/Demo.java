1a6a8d1e9b1047c185c7c11c60ea822b3c3fef01
RutuBagade <rutubagade9@gmail.com>
2023-11-03 17:10:14 +0530

    third commit

--- src/main/java/com/test/Demo.java
+++ src/main/java/com/test/Demo.java
@@ -2,19 +2,21 @@ package com.test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class Demo {
 
 	public static void main(String[] args) {
-       System.out.println("ArrayList Examples----");
+		System.out.println("ArrayList Examples----");
 		ArrayList<String> namesList = new ArrayList<String>(Arrays.asList("alex", "brian", "charles"));
 
 		for (String name : namesList) {
 			System.out.println(name);
 		}
-		
+
 		System.out.println("LinkedList examples---");
 		LinkedList<String> al = new LinkedList<String>();// creating linked list
 		al.add("Rachit"); // adding elements
@@ -24,6 +26,16 @@ public class Demo {
 		while (itr.hasNext()) {
 			System.out.println(itr.next());
 		}
-	}
+		System.out.println("Hashmap Examples :------");
+		HashMap<Integer, String> map = new HashMap<Integer, String>();// Creating HashMap
+		map.put(1, "Mango"); // Put elements in Map
+		map.put(2, "Apple");
+		map.put(3, "Banana");
+		map.put(4, "Grapes");
 
+		System.out.println("Iterating Hashmap...");
+		for (Map.Entry m : map.entrySet()) {
+			System.out.println(m.getKey() + " " + m.getValue());
+		}
+	}
 }
